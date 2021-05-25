2723da270fb4ee31002187ce32381c3e99ec5f3e
dportoles <dporto@gmail.com>
2021-05-25 22:39:42 +0200

    Doc-html actualizada con widoco

---
+++ widoco.bat
@@ -0,0 +1,8 @@
+"C:\Program Files\Java\jre1.8.0_221\bin\java" -jar widoco-1.4.15-jar-with-dependencies.jar -oops -ontFile cos\cos-core.ttl -webVowl  -rewriteAll -lang en-es-pt -outFolder doc_html\cos
+"C:\Program Files\Java\jre1.8.0_221\bin\java" -jar widoco-1.4.15-jar-with-dependencies.jar -oops -ontFile iberian\ifi-core.ttl -webVowl  -rewriteAll -lang en-es-pt -outFolder doc_html\iberian\ifi
+"C:\Program Files\Java\jre1.8.0_221\bin\java" -jar widoco-1.4.15-jar-with-dependencies.jar -oops -ontFile iberian\ilu-core.ttl -webVowl  -rewriteAll -lang en-es-pt -outFolder doc_html\iberian\ilu
+"C:\Program Files\Java\jre1.8.0_221\bin\java" -jar widoco-1.4.15-jar-with-dependencies.jar -oops -ontFile ifn\ifn-core.ttl -webVowl  -rewriteAll -lang en-es -outFolder doc_html\ifn
+"C:\Program Files\Java\jre1.8.0_221\bin\java" -jar widoco-1.4.15-jar-with-dependencies.jar -oops -ontFile ifn-pt\ifn-pt-core.ttl -webVowl  -rewriteAll -lang en-es-pt -outFolder doc_html\ifn-pt
+"C:\Program Files\Java\jre1.8.0_221\bin\java" -jar widoco-1.4.15-jar-with-dependencies.jar -oops -ontFile mfe\mfe-core.ttl -webVowl  -rewriteAll -lang en-es -outFolder doc_html\mfe
+"C:\Program Files\Java\jre1.8.0_221\bin\java" -jar widoco-1.4.15-jar-with-dependencies.jar -oops -ontFile measures\measures-core.ttl -webVowl  -rewriteAll -lang en-es -outFolder doc_html\measures
+"C:\Program Files\Java\jre1.8.0_221\bin\java" -jar widoco-1.4.15-jar-with-dependencies.jar -oops -ontFile position\position-core.ttl -webVowl  -rewriteAll -lang en-es -outFolder doc_html\position
